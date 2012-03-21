645e2dfd8eb1a5df0d7f165c37a1090fd7b50f6b
Rahul Kumar <rahulsinner@gmail.com>
2012-03-21 21:39:24 +0530

    Performant gevent server.

--- memg_gevent_server.py
+++ memg_gevent_server.py
@@ -1,37 +1,14 @@
 #!/usr/bin/env python
-import gevent
-from gevent import socket
-import sys
+from gevent.server import StreamServer
 
-CACHE = {}
-
-
-def main():
-    sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    sock.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
-    sock.bind(("127.0.0.1", 11211))
-    sock.listen(1)
-
-    if '--single' in sys.argv:
-        conn, client = sock.accept()
-        handle_con(conn, client)
-    else:
-        server = gevent.server.StreamServer(sock, handle_con)
-        server.start_accepting()
+cache = {}
 
 def handle_con(conn, client):
-
-    try:
-        # Disable universal new lines for python 2 compatibility
-        sockfile = conn.makefile(newline="")
-    except TypeError:
-        # python 2
-        sockfile = conn.makefile()
-
+    sockfile = conn.makefile()
     while True:
-
         line = sockfile.readline()
-        if line == "":
+        if line == "quit\r\n":
+            sockfile.close()
             break
 
         parts = line.split()
@@ -39,30 +16,21 @@ def handle_con(conn, client):
 
         if cmd == "get":
             key = parts[1]
-
-            try:
-                val = CACHE[key]
-                output(conn, "VALUE %s 0 %d\r\n" % (key, len(val)))
-                output(conn, val + "\r\n")
-            except KeyError:
-                pass
-            output(conn, "END\r\n")
-
+            buffer = []
+            if cache.get(key):
+                val = cache[key]
+                buffer.append("VALUE %s 0 %d\r\n" % (key, len(val)))
+                buffer.append("%s\r\n" % val)
+            buffer.append("END\r\n")
+            sockfile.write(''.join(buffer))
+            sockfile.flush()
         elif cmd == "set":
             key = parts[1]
-            #exp = parts[2]
-            #flags = parts[3]
             length = int(parts[4])
             val = sockfile.read(length + 2)[:length]
-            CACHE[key] = val
-
-            output(conn, "STORED\r\n")
-
-
-def output(conn, string):
-    """Actually write to socket"""
-    conn.sendall(string.encode("utf8"))
-
+            cache[key] = val
+            sockfile.write("STORED\r\n")
+            sockfile.flush()
 
 if __name__ == "__main__":
-    main()
+    StreamServer(("127.0.0.1", 11211), handle_con).serve_forever()
