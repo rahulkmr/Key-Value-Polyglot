66adaae6b76470ae805ad9f3868ea3357340211f
Rahul Kumar <rahulsinner@gmail.com>
2012-03-21 20:20:02 +0530

    Cleaning up code and fixing weird memcache error.

--- memg_epoll.py
+++ memg_epoll.py
@@ -1,12 +1,11 @@
 #!/usr/bin/env python
-import os
 import select
 import socket
 from collections import defaultdict
 
 cache = {}
 writes = defaultdict(list)
-fd_to_sock = {}
+fd_to_file = {}
 
 def _server_socket():
     sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
@@ -26,28 +25,22 @@ def main():
         for fd, event in ready:
             if fd == sock_fd:
                 conn, _ = sock.accept()
-                fd_to_sock[conn.fileno()] = conn
+                fd_to_file[conn.fileno()] = conn.makefile()
                 epoll.register(conn, select.EPOLLIN | select.EPOLLOUT)
             else:
+                sockfile = fd_to_file[fd]
                 if event & select.EPOLLOUT:
                     if writes[fd]:
-                        sock = fd_to_sock[fd]
-                        sock.sendall(''.join(writes[fd]))
+                        sockfile.write(''.join(writes[fd]))
+                        sockfile.flush()
                         writes[fd] = []
                 if event & select.EPOLLIN:
-                    sockfile = _make_file(fd)
                     line = sockfile.readline()
-                    if line == "":
+                    if line == 'quit\r\n':
                         epoll.unregister(fd)
+                        sockfile.close()
                         continue
                     handle_read(fd, line, sockfile)
-
-def _make_file(fd):
-    if _make_file.files.get(fd):
-        return _make_file.files[fd]
-    _make_file.files[fd] = os.fdopen(fd)
-    return _make_file.files[fd]
-_make_file.files = {}
 
 def handle_read(conn, line, sockfile):
     parts = line.split()
