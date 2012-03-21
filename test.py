daae67438a363ead57bfd4d7a0dfa5fad6e40963
Rahul Kumar <rahulsinner@gmail.com>
2012-03-21 15:19:33 +0530

    Change 500 gets and sets to 10000.

--- test.py
+++ test.py
@@ -2,14 +2,10 @@ import pylibmc
 
 mc = pylibmc.Client(["127.0.0.1"])
 
-# Check \r\n in data
-mc.set('br', 'one\r\ntwo')
-assert(mc.get('br') == 'one\r\ntwo')
-
 # set
-for i in range(500):
+for i in range(10000):
     mc.set(str(i), i) # This hashmap syntax writes to memcached
 # get
-for i in range(500):
+for i in range(10000):
     val = mc.get(str(i))        # Hashmap syntax reads from memcached
     assert(int(val) == i)
