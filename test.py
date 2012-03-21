1b04823c2244b963a1408ea1220245243e71f6a6
Rahul Kumar <rahulsinner@gmail.com>
2012-03-21 15:03:45 +0530

    Modifying for my older version of pylibmc

--- test.py
+++ test.py
@@ -2,21 +2,14 @@ import pylibmc
 
 mc = pylibmc.Client(["127.0.0.1"])
 
-# Check empty behavior
-try:
-    mc[str(-1)]
-    assert(False)
-except KeyError:
-    pass
-
 # Check \r\n in data
-mc['br'] = 'one\r\ntwo'
-assert(mc['br'] == 'one\r\ntwo')
+mc.set('br', 'one\r\ntwo')
+assert(mc.get('br') == 'one\r\ntwo')
 
 # set
 for i in range(500):
-    mc[str(i)] = i          # This hashmap syntax writes to memcached
+    mc.set(str(i), i) # This hashmap syntax writes to memcached
 # get
 for i in range(500):
-    val = mc[str(i)]        # Hashmap syntax reads from memcached
+    val = mc.get(str(i))        # Hashmap syntax reads from memcached
     assert(int(val) == i)
